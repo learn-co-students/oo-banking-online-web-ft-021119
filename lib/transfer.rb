156c6f204ae804cc3f001470f39033b91fefe4f1
github username <github email address>
2019-02-28 11:52:08 -0500

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,12 @@
+require 'pry'
+
 class Transfer
   # your code here
+
+  def initializes(sender, transfer, num)
+      @sender = sender
+      @transfer = transfer
+      @num = num
+      binding.pry
+  end
 end
