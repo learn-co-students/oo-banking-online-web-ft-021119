51246dc2557b48b02f9fd7b6696eaeff4003cbf3
Jacqi Entzminger <jackie.entz@gmail.com>
2019-03-05 03:55:08 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,26 @@
 class BankAccount
+  attr_reader :name 
+  attr_accessor :balance, :status
+  
+  def initialize(name)
+    @balance = 1000
+    @name = name 
+    @status = 'open' 
+  end 
+  
+  def deposit
+    
+  end 
 
+  def display_balance
+    
+  end 
+  
+  def valid?
+    
+  end 
+  
+  def close_account
+    
+  end 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,23 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :amount 
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = 'pending' 
+    @amount = amount 
+  
+  end 
+
+  def valid?
+    
+  end 
+  
+  def execute_transaction
+    
+  end 
+
+  def reverse_transfer
+    
+  end 
 end
