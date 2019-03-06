4e25acd8af82ce7cec130c6aba8caa88c76a78fb
Taylor Swinton <taylorswinton1@gmail.com>
2019-03-06 01:04:25 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,47 @@
 class Transfer
-  # your code here
+
+#WHAT TRANSFER SHOULD BE ALLOWED TO DO
+#Transfer should start in a pending status
+#Then go to complete state OR rejected status if the accounts aren't valid
+  #validty requires balance > 0, and status = open
+#Once completed it can be put into a reversed status.
+
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    if @sender.valid? == true && @receiver.valid? == true
+      true
+    else
+      false
+    end
+  end
+
+  def execute_transaction
+    if @sender.balance < @amount || @sender.status == "closed"
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    elsif @status == "complete"
+      return @status
+    else
+      @sender.deposit(@amount * -1)
+      @receiver.deposit(@amount)
+      @status = "complete"
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      @receiver.deposit(@amount * -1)
+      @sender.deposit(@amount)
+      @status = "reversed"
+    end
+  end
+
 end
